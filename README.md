efed27fb69a153ff5b6c695c2a3ba318efb11160
胡椒粉 <1198754948@qq.com>
2021-11-22 14:46:47 +0800

    upd ReadMe

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ Check out my articles:
 [Part 1: Build a web editor with syntax colorization](https://medium.com/better-programming/create-a-custom-web-editor-using-typescript-react-antlr-and-monaco-editor-part-1-2f710c69c18c)  
 [Part 2: Implement language services, auto-completion, syntax and semantic validation and auto-formatting](https://medium.com/better-programming/create-a-custom-web-editor-using-typescript-react-antlr-and-monaco-editor-bcfc7554e446)  
 
+## 关于本项目
+在TodoLangEditor的基础上，实现了自定义的Excel公式解析器  
 
 # 特殊函数
 IF 和 SWITCH 返回类型UNKNOW  
--- src/tbexp-lang/schemaMap.data.ts
+++ src/tbexp-lang/schemaMap.data.ts
@@ -14,26 +14,6 @@ export class ContextResource {
   constructor() {}
   public localVariable: Record<string, IFieldMeta>;
   public globalVariables: Record<string, IFieldMeta>;
-  // private _localVariables: Record<string, IFieldMeta>;
-  // private _globalVariables: Record<string, IFieldMeta>;
-
-  // get getLocalVariables(): Record<string, IFieldMeta>{
-  //   return this._localVariables;
-  // }
-  // set setLocalVariables(map: Record<string, IFieldMeta>) {
-  //   this._localVariables = map;
-  // }
-
-  // get getGlobalVariables(): Record<string, IFieldMeta> {
-  //   return this._globalVariables;
-  // }
-  // set setGlobalVariables(map: Record<string, IFieldMeta>) {
-  //   this._globalVariables = map;
-  // }
-
-  // getGlobalSchema =()=>{
-  //   return this.getGlobalVariables;
-  // }
 }
 
 export const getVariableType = (
@@ -42,7 +22,6 @@ export const getVariableType = (
   type: number = 0,
 ) => {
   let arr = text.replace('!', '').replace('$', '').split('.');
-  // text.replace(/^\{!/gi, '').replace(/\}$/gi, '').replace('$', '').split('.');
   arr.forEach((v, i, arr) => {
     if (v.indexOf('[') > -1) {
       arr[i] = v.substring(0, v.indexOf('['));
@@ -66,7 +45,6 @@ export const getVariableType = (
 export const getPathMeta = (
   fieldMeta: IFieldMeta,
   path: string[],
-  // getRemoteSchema: GetRemoteSchema,
 ): IFieldMeta | any | undefined => {
   if (path.length === 1) {
     if (fieldMeta.type === MetaValueType.ARRAY) {
@@ -106,7 +84,6 @@ export const getPathMeta = (
 export const getPathMetaAsync = async (
   fieldMeta: IFieldMeta,
   path: string[],
-  // getRemoteSchema: GetRemoteSchema,
 ): Promise<IFieldMeta | any | undefined> => {
   if (path.length === 1) {
     if (fieldMeta.type === MetaValueType.ARRAY) {
@@ -140,253 +117,5 @@ export const getPathMetaAsync = async (
   if (currentMeta == null) {
     return undefined;
   }
-  return await getPathMetaAsync(currentMeta, path);//, getRemoteSchema);
+  return await getPathMetaAsync(currentMeta, path);
 };
-
-// export const schemaMap: Record<string, IFieldMeta> = {
-//   currentUser: {
-//     key: '123',
-//     name: 'currentUser',
-//     type: 'object',
-//     properties: {
-//       id: {
-//         key: 'id',
-//         name: 'ID',
-//         type: 'number',
-//       },
-//       name: {
-//         key: 'name',
-//         name: 'Name',
-//         type: 'string',
-//       },
-//       project: {
-//         key: 'project',
-//         name: 'Project',
-//         type: 'object',
-//         properties: {
-//           id2: {
-//             key: 'id2',
-//             name: 'ID2',
-//             type: 'number',
-//           },
-//           name2: {
-//             key: 'name2',
-//             name: 'Name2',
-//             type: 'string',
-//           },
-//           project2: {
-//             key: 'project2',
-//             name: 'Project2',
-//             type: 'object',
-//             properties: {
-//               id3: {
-//                 key: 'id3',
-//                 name: 'ID3',
-//                 type: 'number',
-//               },
-//               name3: {
-//                 key: 'name3',
-//                 name: 'Name3',
-//                 type: 'string',
-//               },
-//             },
-//           },
-//         },
-//       },
-//       books: {
-//         key: '444',
-//         name: 'books',
-//         type: 'array',
-//         items: {
-//           type: 'object',
-//           properties: {
-//             price: {
-//               key: 'price',
-//               type: 'number',
-//               name: 'price',
-//             },
-//             authors: {
-//               key: 'authors',
-//               type: 'array',
-//               name: 'authors',
-//               items: {
-//                 type: 'object',
-//                 properties: {
-//                   name: {
-//                     key: 'name',
-//                     type: 'string',
-//                     name: 'name',
-//                   },
-//                   age: {
-//                     key: 'age',
-//                     type: 'integer',
-//                     name: 'age',
-//                   },
-//                 },
-//               },
-//             },
-//           },
-//         },
-//       },
-//     },
-//   },
-//   tags: {
-//     key: '333',
-//     name: 'tags',
-//     type: 'array',
-//     items: {
-//       type: 'string',
-//     },
-//     required: true,
-//   },
-//   books: {
-//     key: '444',
-//     name: 'books',
-//     type: 'array',
-//     items: {
-//       type: 'object',
-//       properties: {
-//         price: {
-//           key: 'price',
-//           type: 'number',
-//           name: 'price',
-//         },
-//         title: {
-//           key: 'title',
-//           type: 'string',
-//           name: 'title',
-//         },
-//         authors: {
-//           key: 'authors',
-//           type: 'array',
-//           name: 'authors',
-//           items: {
-//             type: 'object',
-//             properties: {
-//               name: {
-//                 key: 'name',
-//                 type: 'string',
-//                 name: 'name',
-//               },
-//               country: {
-//                 key: 'country',
-//                 type: 'string',
-//                 name: 'country',
-//               },
-//             },
-//           },
-//         },
-//       },
-//     },
-//   },
-//   //外键关联
-//   // ObjectRefId:{
-//   // },
-//   //多维数组，meta-schema lib不支持
-//   // ManyDiemension: {
-//   //   key: '333',
-//   //   name: 'ManyDiemension',
-//   //   type: 'array',
-//   //   items: {
-//   //     type: 'array',
-//   //     items: {
-//   //       type: 'string'
-//   //     }
-//   //   },
-//   //   required: true
-//   // },
-// };
-
-// export const localSchemaMap: Record<string, IFieldMeta> = {
-//   address: {
-//     key: 'address',
-//     name: 'Address',
-//     type: 'object',
-//     properties: {
-//       aid: {
-//         key: 'aid',
-//         name: 'ID',
-//         type: 'number',
-//       },
-//       aname: {
-//         key: 'aname',
-//         name: 'Name',
-//         type: 'string',
-//       },
-//       aproject: {
-//         key: 'aproject',
-//         name: 'Project',
-//         type: 'object',
-//         properties: {
-//           aid2: {
-//             key: 'aid2',
-//             name: 'ID2',
-//             type: 'number',
-//           },
-//           aname2: {
-//             key: 'aname2',
-//             name: 'Name2',
-//             type: 'string',
-//           },
-//           aproject2: {
-//             key: 'aproject2',
-//             name: 'Project2',
-//             type: 'object',
-//             properties: {
-//               aid3: {
-//                 key: 'aid3',
-//                 name: 'ID3',
-//                 type: 'number',
-//               },
-//               aname3: {
-//                 key: 'aname3',
-//                 name: 'Name3',
-//                 type: 'string',
-//               },
-//             },
-//           },
-//         },
-//       },
-//       abooks: {
-//         key: '444',
-//         name: 'abooks',
-//         type: 'array',
-//         items: {
-//           type: 'object',
-//           properties: {
-//             aprice: {
-//               key: 'aprice',
-//               type: 'number',
-//               name: 'price',
-//             },
-//             aauthors: {
-//               key: 'aauthors',
-//               type: 'array',
-//               name: 'authors',
-//               items: {
-//                 type: 'object',
-//                 properties: {
-//                   aname: {
-//                     key: 'aname',
-//                     type: 'string',
-//                     name: 'name',
-//                   },
-//                   age: {
-//                     key: 'age',
-//                     type: 'integer',
-//                     name: 'age',
-//                   },
-//                 },
-//               },
-//             },
-//           },
-//         },
-//       },
-//     },
-//   }
-// };
-
-/**
- * type =0 全局变量 1局部变量
-*/
-
